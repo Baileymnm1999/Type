219c7fed93e6e0f7b6c797e69aef7e573716de0d
Bailey <Bailey.Mccarty.Morgan@gmail.com>
2018-04-05 14:20:01 -0400

    Added the teaching slideshow portion

--- workspace/baileyTest.m
+++ workspace/baileyTest.m
@@ -22,7 +22,7 @@ function varargout = baileyTest(varargin)
 
 % Edit the above text to modify the response to help baileyTest
 
-% Last Modified by GUIDE v2.5 05-Apr-2018 12:24:39
+% Last Modified by GUIDE v2.5 05-Apr-2018 13:22:08
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,6 +54,9 @@ function baileyTest_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for baileyTest
 handles.output = hObject;
+handles.slideCount = 1;
+axis(handles.slide);
+imshow('../res/img/1.jpg');
 
 % Update handles structure
 guidata(hObject, handles);
@@ -71,3 +74,96 @@ function varargout = baileyTest_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+
+% --- Executes on button press in nextSlideButt.
+function nextSlideButt_Callback(hObject, eventdata, handles)
+% hObject    handle to nextSlideButt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+    axis(handles.slide);
+    switch(handles.slideCount)
+        case 1
+            cla
+            imshow('../res/img/2.png')
+            handles.slideCount = 2;
+        case 2
+            cla
+            imshow('../res/img/3.png')
+            handles.slideCount = 3;
+        case 3
+            cla
+            imshow('../res/img/4.jpg');
+            handles.slideCount = 4;
+        case 4
+            cla
+            imshow('../res/img/5.jpg');
+            handles.slideCount = 5;
+        case 5
+            imshow('../res/img/6.jpg');
+            handles.slideCount = 6;
+        case 6
+            imshow('../res/img/7.jpg');
+            handles.slideCount = 7;
+        case 7
+            imshow('../res/img/8.jpg');
+            handles.slideCount = 8;
+        case 8
+            imshow('../res/img/9.jpg');
+            handles.slideCount = 9;
+        case 9
+            imshow('../res/img/10.jpg');
+            handles.slideCount = 10;
+        case 10
+            imshow('../res/img/11.jpg');
+            handles.slideCount = 11;
+        case 11
+            imshow('../res/img/11.jpg');
+            handles.slideCount = 11;
+    
+    end
+    guidata(hObject, handles);
+
+
+% --- Executes on button press in previousSlideButt.
+function previousSlideButt_Callback(hObject, eventdata, handles)
+% hObject    handle to previousSlideButt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    axis(handles.slide);
+    switch(handles.slideCount)
+        case 1
+            imshow('../res/img/1.jpg');
+            handles.slideCount = 1;
+        case 2
+            imshow('../res/img/1.jpg');
+            handles.slideCount = 1;
+        case 3
+            imshow('../res/img/2.png');
+            handles.slideCount = 1;
+        case 4
+            imshow('../res/img/3.png');
+            handles.slideCount = 2;
+        case 5
+            imshow('../res/img/4.jpg');
+            handles.slideCount = 3;
+        case 6
+            imshow('../res/img/5.jpg');
+            handles.slideCount = 4;
+        case 7
+            imshow('../res/img/6.jpg');
+            handles.slideCount = 5;
+        case 8
+            imshow('../res/img/7.jpg');
+            handles.slideCount = 7;
+        case 9
+            imshow('../res/img/8.jpg');
+            handles.slideCount = 8;
+        case 10
+            imshow('../res/img/9.jpg');
+            handles.slideCount = 9;
+        case 11
+            imshow('../res/img/10.jpg');
+            handles.slideCount = 10;
+    end
+    guidata(hObject, handles);
