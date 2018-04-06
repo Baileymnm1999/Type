45a47fcc4099925341509feee01bfecf5833dc55
Sancho Montana <snidersc@mail.uc.edu>
2018-04-06 13:25:46 -0400

    Created a way to reset the typing box if its string equals the fisrt word in the copyPasta

--- workspace/typetest.m
+++ workspace/typetest.m
@@ -1,35 +1,37 @@
-function varargout = spencerTest(varargin)
-% SPENCERTEST MATLAB code for spencerTest.fig
-%      SPENCERTEST, by itself, creates a new SPENCERTEST or raises the existing
+
+
+function varargout = typetest(varargin)
+% TYPETEST MATLAB code for typetest.fig
+%      TYPETEST, by itself, creates a new TYPETEST or raises the existing
 %      singleton*.
 %
-%      H = SPENCERTEST returns the handle to a new SPENCERTEST or the handle to
+%      H = TYPETEST returns the handle to a new TYPETEST or the handle to
 %      the existing singleton*.
 %
-%      SPENCERTEST('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in SPENCERTEST.M with the given input arguments.
+%      TYPETEST('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TYPETEST.M with the given input arguments.
 %
-%      SPENCERTEST('Property','Value',...) creates a new SPENCERTEST or raises the
+%      TYPETEST('Property','Value',...) creates a new TYPETEST or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before spencerTest_OpeningFcn gets called.  An
+%      applied to the GUI before typetest_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to spencerTest_OpeningFcn via varargin.
+%      stop.  All inputs are passed to typetest_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help spencerTest
+% Edit the above text to modify the response to help typetest
 
-% Last Modified by GUIDE v2.5 05-Apr-2018 13:31:44
+% Last Modified by GUIDE v2.5 06-Apr-2018 13:22:44
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @spencerTest_OpeningFcn, ...
-                   'gui_OutputFcn',  @spencerTest_OutputFcn, ...
+                   'gui_OpeningFcn', @typetest_OpeningFcn, ...
+                   'gui_OutputFcn',  @typetest_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,46 +46,45 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before spencerTest is made visible.
-function spencerTest_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before typetest is made visible.
+function typetest_OpeningFcn(hObject, eventdata, handles, varargin)
+global loadText;
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to spencerTest (see VARARGIN)
+% varargin   command line arguments to typetest (see VARARGIN)
 
-% Choose default command line output for spencerTest
+% Choose default command line output for typetest
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-apache = fileread('../res/copyPastas/apache.txt');
-beeMovie = fileread('../res/copyPastas/beeMovie.txt');
-navySeal = fileread('../res/copyPastas/navySeal.txt');
-safetyDance = fileread('../res/copyPastas/safetyDance.txt');
-tragedy = fileread('../res/copyPastas/tragedy.txt');
 rng shuffle;
-j = randi(5);
-switch(j)
-    case 1
-        set(handles.prompt, 'string', apache);
+i = randi(5);
+switch i
+    case 1 
+        loadText = fileread('../res/copyPastas/apache.txt');
     case 2
-        set(handles.prompt, 'string', beeMovie);
+        loadText = fileread('../res/copyPastas/beeMovie.txt');
     case 3
-        set(handles.prompt, 'string', navySeal);
+        loadText = fileread('../res/copyPastas/navySeal.txt');
     case 4
-        set(handles.prompt, 'string', safetyDance);
+        loadText = fileread('../res/copyPastas/safetyDance.txt');
     case 5
-        set(handles.prompt, 'string', tragedy);
+        loadText = fileread('../res/copyPastas/tragedy.txt');
 end
+set(handles.story, 'string', loadText);
+loadText = string(loadText).split(' ');
+disp(loadText(10));
 
-% UIWAIT makes spencerTest wait for user response (see UIRESUME)
+% UIWAIT makes typetest wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = spencerTest_OutputFcn(hObject, eventdata, handles) 
+function varargout = typetest_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -93,13 +94,18 @@ function varargout = spencerTest_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
+
 function interaction_Callback(hObject, eventdata, handles)
+global loadText;
 % hObject    handle to interaction (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hints: get(hObject,'String') returns contents of interaction as text
 %        str2double(get(hObject,'String')) returns contents of interaction as a double
+if get(handles.interaction, 'string') == loadText(1) || get(handles.interaction, 'string') == "Press [ENTER] to Begin" 
+    set(handles.interaction, 'string', '');
+end
 
 
 % --- Executes during object creation, after setting all properties.
@@ -113,3 +119,11 @@ function interaction_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over interaction.
+function interaction_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to interaction (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
